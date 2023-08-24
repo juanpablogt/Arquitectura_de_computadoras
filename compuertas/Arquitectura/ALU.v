daffb26ba794dd237df32d77bdcd82b96e85af0d
JPablo <juapabgonzalez@utp.edu.co>
2023-08-23 23:18:44 -0500

    algunos ajustes al opcode

--- compuertas/Arquitectura/ALU.v
+++ compuertas/Arquitectura/ALU.v
@@ -6,13 +6,13 @@ always @(a,b,opcode)
     case(opcode)
         4'b0011: x = a & b;
         4'b0100: x = a | b;
-        4'b0101: x = a ^ b;
+        4'b0111: x = a ^ b;
         4'b0110: x[0] = a > b;
         4'b0111: x[0] = a < b;
-        4'b1010: {y[0],x} = a + b;
-        4'b1011: X= a - b;
-        4'b1101: {y,x} = a >> b;
-        4'b1110: {y,x} = a << b;
+        4'b0000: {y[0],x} = a + b;
+        4'b1000: X= a - b;
+        4'b0101: {y,x} = a >> b;
+        4'b0001: {y,x} = a << b;
         deault: $display("error");
     endcase
 endmodule
